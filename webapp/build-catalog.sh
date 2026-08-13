aba4823b08e05ce54c58a9636cfdaf5b0433c8cc
ArrowSK <56678503+ArrowSK@users.noreply.github.com>
2026-08-13 16:14:04 +0200

    Pin OpenNGC for web catalogue

--- webapp/build-catalog.sh
+++ webapp/build-catalog.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
 set -eu
+commit="da90466031b0372c896588b85be6016c617e205b"
+curl --fail --silent --show-error --location "https://raw.githubusercontent.com/mattiaverga/OpenNGC/${commit}/database_files/NGC.csv" -o NGC.csv
+curl --fail --silent --show-error --location "https://raw.githubusercontent.com/mattiaverga/OpenNGC/${commit}/database_files/addendum.csv" -o addendum.csv
+python3 build_catalog.py NGC.csv addendum.csv catalog.tsv
