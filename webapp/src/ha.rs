c7be3ac7d32be2c88125a1a803317d71934d478a
ArrowSK <56678503+ArrowSK@users.noreply.github.com>
2026-08-13 16:03:09 +0200

    Complete web location adapter

--- webapp/src/ha.rs
+++ webapp/src/ha.rs
@@ -2,11 +2,34 @@ use crate::error::AppResult;
 use crate::models::Location;
 
 #[derive(Clone)]
-pub struct HaClient;
+pub struct HaClient {
+    latitude: f64,
+    longitude: f64,
+    elevation_m: f64,
+    label: String,
+    timezone: String,
+}
 
 impl HaClient {
+    pub fn for_location(value: &Location) -> Self {
+        Self {
+            latitude: value.latitude,
+            longitude: value.longitude,
+            elevation_m: value.elevation_m,
+            label: value.label.clone(),
+            timezone: value.timezone.clone(),
+        }
+    }
+
     pub fn location(&self, _person: &str) -> AppResult<Location> {
-        todo!()
+        Ok(Location {
+            latitude: self.latitude,
+            longitude: self.longitude,
+            elevation_m: self.elevation_m,
+            label: self.label.clone(),
+            timezone: self.timezone.clone(),
+            source: "web_request".to_string(),
+        })
     }
 
     pub fn numeric_state(&self, _entity_id: &str) -> Option<f64> {
